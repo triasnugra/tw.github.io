317d57a3e36dc1d8d99c92600e281fc40b6b8248
trias <triasnugra97@gmail.com>
2023-11-17 14:31:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function updateDateTime() {
   const now = new Date();
   
-  const dateElement = document.getElementById('date');
-  const timeElement = document.getElementById('time');
+  const currentDateElement = document.getElementById('current-date');
   const countdownMonthElement = document.getElementById('countdown-month');
   const countdownYearElement = document.getElementById('countdown-year');
   const quoteElement = document.getElementById('quote');
 
-  // Update date and time
-  dateElement.textContent = now.toLocaleDateString('en-US');
-  timeElement.textContent = now.toLocaleTimeString('en-US');
+  // Update current date and time
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  currentDateElement.textContent = now.toLocaleDateString('en-US', options);
 
   // Calculate countdown for the end of the month
   const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
